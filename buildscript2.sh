f37eda5aa17219e2ef7f756814033f1fe8960b67
Caden Hong <caden.p.hong@gmail.com>
2022-08-11 12:31:47 -0400

    Final version to submit to Tyrone

--- buildscript2.sh
+++ buildscript2.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
 
-##############################################################################################
+####################################################################################################################################
+#
 # Name: Caden Hong
+#
 # Date: August 9, 2022
 #
-# wc -l organized.csv | awk '{print $1} _> first part gives out number and file name, so I awked only the number
-# if in awk -> numeric comparison
+# Description: This script will automatically check the server's processes and ask user to terminate a process if memory > 350 MB
+#	- Check for processes using ps aux, then sort it by rss column (i.e. memory in KB), and save result to processes.txt
+#	- In awk, use a conditional to find all processes that exceeds 350 MB in memory usage, then save the PIDs and converted
+#	  memory (from KB to MB) to organized.csv
+#	- If there are any processes saved in organized.csv, display the number of processes with a warning message
+#		- Case statement (inside a while loop) to ask for user's input on whether they want to terminate those processes
+#			- If input is "n" or "N", exit the program with a warning message
+#			- If input is "y" or "Y", break case and move to the next section
+#			- If input is anything else, while loop will repeat
+#		- For loop to read through organized.csv and display PID, name (found with "ps -p $pid -o comm="), and memory
+#		- Generate a separate kill.txt file that only contains the PID
+#		- While loop to prompt user to input the PID of process they want to terminate
+#			- If valid PID (checked against kill.txt with grep), use "kill -15 $input2" to terminate the process
+#			- If input is "q" or "Q", break out of loop
+#			- If invalid input, the loop will repeat
+#		- Exit the program with message "System check completed"
+#	- Else, exit the program with message "... no system processes exceeding 350 MB of memory usage"
 #
-# potential error: if I assign 350 to a value and replace it in the if statement it won't run correctly
-#############################################################################################
+# Potential Error:
+#	1. If 350 (or any limit for MB) is assigned to a variable and it is used in line 2, the if statement will not run correctly
+# 	2. Even after all the processes are terminated, the last while loop will continue to ask for a PID to terminate -
+#	   only way to exit the script at this point is to enter "q" or "Q" to quit
+#
+####################################################################################################################################
 
 ps aux --sort -rss > processes.txt
-#echo "PID,RSS,MEM_Converted" > organized.csv
-awk '{ if (($6/1024) > 350) {print $2, $6, ($6/1024)} }' processes.txt | tr ' ' ',' > organized.csv
+awk '{ if (($6/1024) > 350) {print $2, ($6/1024)} }' processes.txt | tr ' ' ',' > organized.csv
 
-echo $'\nChecking system processes and performance...\n'
-# sleep 2
+echo "---------------------------------------------------------"
+echo $'Checking system processes and performance...\n'
+sleep 2
 
 if [ $(wc -l organized.csv | awk '{print $1}') -ge 1 ]; then
-  echo "WARNING! Total of $(($(wc -l organized.csv | awk '{print $1}')-1)) process(es) exceeding 350 MB of memory."
+  echo "WARNING! Total of $(wc -l organized.csv | awk '{print $1}') process(es) exceeding 350 MB of memory."
 
   while true;
   do
     read -p $'\nWould you like to terminate these processes? (y/n): ' input
 
     case $input in
-      n | N) echo $'Exiting without terminating any processes...\nWARNING! Your performance may be affected.\n' ;;
       y | Y) break ;;
+      n | N) echo $'Exiting without terminating processes...\n\nWARNING! Performance may be affected.\n---------------------------------------------------------' ;;
       *) echo "Invalid input!!" ;;
     esac
 
@@ -40,18 +61,12 @@ if [ $(wc -l organized.csv | awk '{print $1}') -ge 1 ]; then
   for line in $(cat organized.csv)
   do
     pid=$(awk -F, '{print $1}' <<< $line)
-    mb=$(awk -F, '{print $3}' <<< $line)
+    mb=$(awk -F, '{print $2}' <<< $line)
     name=$(ps -p $pid -o comm=)
     echo "  PID $pid ($name), $mb MB"
   done
 
-
-#  awk -F, '{print $1}' organized.csv > kill.txt
-#  for line in $(cat kill.txt)
-#  do
-#    name=$(ps -p $line -o comm=)
-#    echo "$name (PID: $line)"
-#  done
+  awk -F, '{print $1}' organized.csv > kill.txt
 
   while true;
   do
@@ -68,10 +83,12 @@ if [ $(wc -l organized.csv | awk '{print $1}') -ge 1 ]; then
   done
 
   sleep 1
-  echo $'\nSystem check completed.\n'
+  echo $'\nSystem check completed.'
+  echo "---------------------------------------------------------"
   exit
 
 else
-  echo $'There are no system processes exceeding 350MB of memory.\n'
+  echo "There are no system processes exceeding 350MB of memory usage."
+  echo $'---------------------------------------------------------\n'
 fi
 exit
